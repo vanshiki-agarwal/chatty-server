671a4a5f61f38748acc2099074e337833b7119c5
vanshiki-agarwal <vanshiki40@gmail.com>
2025-03-01 02:58:27 +0530

    receive and send the received msg

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
-const express = require('express');
-const http = require('http');
+const express = require("express");
+const http = require("http");
 const app = express();
 const server = http.createServer(app);
 
-const {Server} = require('socket.io');
+const { Server } = require("socket.io");
 const io = new Server(server, {
-    cors: {
-        origin: '*',
-    }
-})
+  cors: {
+    origin: "*",
+  },
+});
 
-io.on('connection', (socket) => {
-        console.log(`User is connected with id: ${socket.id}`);
-        socket.on('client ready', (msg)=>{
-            console.log(msg);
-        }) 
-        socket.on('btn clicked', ()=>{
-            console.log('Button clicked');
-            socket.emit('do something')
-
-        })
-        
-})
+io.on("connection", (socket) => {
+  //   socket.emit("connection", "connected");
+  //   console.log(`User is connected with id: ${socket.id}`);
+  //   socket.on("client ready", (msg) => {
+  //     console.log(msg);
+  //   });
+  //   socket.on("btn clicked", () => {
+  //     console.log("Button clicked");
+  //     socket.emit("do something");
+  //   });
+  socket.on("send_message", (msg) => {
+    console.log(msg);
+    socket.broadcast.emit("receive_message", msg);
+    console.log("msg broadcasted");
+  });
+});
 
 server.listen(3001, () => {
-    console.log('Server is running on port 3001');
-})
+  console.log("Server is running on port 3001");
+});
